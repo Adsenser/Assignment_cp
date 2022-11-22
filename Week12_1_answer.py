d80af79643cf8c25dc134125a452ea16ed12172e
Kimtaeyi <111646357+Adsenser@users.noreply.github.com>
2022-11-22 13:25:14 +0900

    Create Week12_1_answer.py
    random 모듈에 포함된 함수를 이용하여 다음 조건에 맞는 난수를 각각 생성하시오. <조건>
    1. 1부터 100사이에서 정수인 난수를 10개 생성하시오.
    2. 0부터 5사이에서 실수인 난수를 10개 생성하시오.
    3. -1부터 1사이에서 실수인 난수를 10개 생성하시오.

---
+++ Week12_1_answer.py
@@ -0,0 +1,9 @@
+import random
+for i in range(10):
+    print(i,random.randint(1,100))
+print()
+for i in range(10):
+    print(i,random.random()*5)
+print()
+for i in range(10):
+    print(i,random.random()*2-1)
